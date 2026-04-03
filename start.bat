1330ecb7cef3bce408dfa3afecf880ff358ab2aa
vain <vainbusinessinfo@gmail.com>
2026-04-03 17:24:32 +0200

    Simplify start.bat by removing npm install
    Removed dependency installation and error handling.

--- start.bat
+++ start.bat
@@ -1,11 +1,4 @@
 @echo off
-echo installing dependencies...
-npm install
-if %errorlevel% neq 0 (
-    echo npm install failed!
-    pause
-    exit /b
-)
 echo starting monetisation manager...
 npm run dev:full
 pause
